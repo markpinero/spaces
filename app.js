9e3f3b03d1cb92665c50bb0952210a5524952f41
Mark Pinero <markspinero@gmail.com>
2016-11-15 13:14:38 -0800

    Changed Foursquare API from Search to Explore

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-// https://api.foursquare.com/v2/venues/search?near=Northridge,+CA&query=mcdonalds&limit=10&client_id=GOCIDB0RL50MCE2X3G2DYRDXRAA1AMG2XF3R3ZJBRRXSNECL&client_secret=FX40PY51ZXGKUH04AWKQAXOUY3KIYSH1OCYNBHQWEY4OZ10N&v=20161110',
+// https://api.foursquare.com/v2/venues/search?near=Northridge,+CA&query=mcdonalds&limit=10&client_id=GOCIDB0RL50MCE2X3G2DYRDXRAA1AMG2XF3R3ZJBRRXSNECL&client_secret=FX40PY51ZXGKUH04AWKQAXOUY3KIYSH1OCYNBHQWEY4OZ10N&v=20161110
+// AIzaSyA6A_ZqMsyzbR0ZHE-j8SScXUyqqTVgFxE
+
+// https://api.foursquare.com/v2/venues/explore?near=Northridge,+CA&query=mcdonalds&limit=10&client_id=GOCIDB0RL50MCE2X3G2DYRDXRAA1AMG2XF3R3ZJBRRXSNECL&client_secret=FX40PY51ZXGKUH04AWKQAXOUY3KIYSH1OCYNBHQWEY4OZ10N&v=20161110
+
 const CLIENT_ID = 'GOCIDB0RL50MCE2X3G2DYRDXRAA1AMG2XF3R3ZJBRRXSNECL';
 const CLIENT_SECRET = 'FX40PY51ZXGKUH04AWKQAXOUY3KIYSH1OCYNBHQWEY4OZ10N';
 
@@ -11,9 +15,9 @@ function panMapResult(lat, lon) {
 
 function selectLocation() {
   var index = $(this).data('venue');
-  var venue = results[index];
-  var lat = venue.location.lat;
-  var lon = venue.location.lng;
+  var venue = results[index].venue;
+  var lat = venue.location["lat"];
+  var lon = venue.location["lng"];
 
   venue.marker.openPopup();
 
@@ -26,26 +30,26 @@ function drawResults(query, location) {
   $('.js-query').text(query);
   $('.js-location-name').text(location);
 
-	$.getJSON('https://api.foursquare.com/v2/venues/search?near=' + location + '&query=' + query + '&limit=10&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&v=20161109',
+  $.getJSON('https://api.foursquare.com/v2/venues/explore?near=' + location + '&query=' + query + '&limit=10&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&v=20161115',
   function(data) {
-  	results = data.response.venues;
-    $.each(results, function(i, venue){
-  		content = $('<li class="listing">' + venue.name + '<br><address>' + venue.location.address + '</address></li>');
+    results = data.response.groups[0].items;
+    $.each(results, function(i, v){
+      content = $('<li class="listing">' + v.venue.name + '<br><address>' + v.venue.location["address"] + '</address></li>');
       content.data('venue', i);
-      var lat = venue.location.lat;
-      var lon = venue.location.lng;
+      var lat = v.venue.location.lat;
+      var lon = v.venue.location.lng;
 
-      venue.marker = L.marker([lat, lon]).addTo(map);
-      venue.marker.bindPopup(venue.name);
+      v.venue.marker = L.marker([lat, lon]).addTo(map);
+      v.venue.marker.bindPopup(v.venue.name);
 
-			content.appendTo("#results");
-		});
-	});
+      content.appendTo("#results");
+    });
+  });
 }
 
 function getIpLocation() {
-	$.getJSON("https://freegeoip.net/json/", function(data) {
-		$('.js-location').val(data.city + ', ' + data.region_code);
+  $.getJSON("http://freegeoip.net/json/", function(data) {
+    $('.js-location').val(data.city + ', ' + data.region_code);
     drawMap(data.latitude, data.longitude);
   });
 }
@@ -61,12 +65,12 @@ function drawMap(lat, lon) {
 }
 
 function startPage() {
-	getIpLocation();
+  getIpLocation();
 }
 
 $('button').on('click', function(e) {
-	e.preventDefault();
-	$('.search').addClass('hide');
+  e.preventDefault();
+  $('.search').addClass('hide');
 
   var query = $('.query').val();
   var location = $('.location').val();
@@ -75,5 +79,5 @@ $('button').on('click', function(e) {
 });
 
 $(function() {
-	startPage();
+  startPage();
 });
