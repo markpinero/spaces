3397391ca659890b15dbcb6f1af49f411f81c469
Mark Pinero <markspinero@gmail.com>
2016-11-21 14:55:50 -0800

    Changed venue styling. Added Google Maps direction links. Randomized search background. Added callbacks for results.

--- js/app.js
+++ js/app.js
@@ -31,31 +31,36 @@ $('#results').on('click', '.listing', selectLocation);
 
 // }
 
-function drawResults() {
+function drawResults(callback) {
   L.circle(loc, 200).addTo(map);
 
-  $.getJSON('https://api.foursquare.com/v2/venues/explore?ll=' + loc["lat"] + ',' + loc["lon"] + '&section=coffee&venuePhotos=1&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&v=20161115',
+  $.getJSON('https://api.foursquare.com/v2/venues/explore?ll=' + loc["lat"] + ',' + loc["lon"] + '&section=coffee&limit=50&radius=8000&sortByDistance=1&venuePhotos=1&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&v=20161115',
   function(data) {
     results = $.grep(data.response.groups[0].items, function(recommendation) {
-      return recommendation.venue.name !== "7-Eleven";
+      return recommendation.venue.categories[0].id === '4bf58dd8d48988d16d941735' || recommendation.venue.categories[0].id === '4bf58dd8d48988d1e0931735';
     }).slice(0, 10);
+    // if(results.length !== 0) {
+    //   $('.js-query').text("testing");
+    // }
     $.each(results, function(i, recommendation){
-      photos = recommendation.venue.photos.groups[0].items[0];
-      content = $('<li class="listing"><div class="venue"><div class="photo"><img src="' + photos.prefix
-        + "width400" + photos.suffix + '"></div><div class="details">' +
-        recommendation.venue.name + '<br><address>' + recommendation.venue.location["address"] + '</address></div></div></li>'
-      );
-      content.data('recommendation', i);
       var lat = recommendation.venue.location.lat;
       var lon = recommendation.venue.location.lng;
+      photos = recommendation.venue.photos.groups[0].items[0];
+      directionsUrl = 'https://www.google.com/maps/dir/' + loc.lat + ',' + loc.lon + '/' + lat + ',' + lon;
+      content = $('<li class="listing"><div class="venue"><div class="photo" style="background:url(' + photos.prefix + 'width400' + photos.suffix + ') center center"><h1>' + recommendation.venue.name + '</h1></div><a href="' + directionsUrl + '" class="directions">Go</a><div class="details"><address>' + recommendation.venue.location.address + '<br>' + recommendation.venue.location.city + ', ' + recommendation.venue.location.state + ' ' + recommendation.venue.location.postalCode + '</address></div></div></li>');
+      content.data('recommendation', i);
 
       recommendation.venue.marker = L.marker([lat, lon]).addTo(map);
-      recommendation.venue.marker.bindPopup(recommendation.venue.name);
+      recommendation.venue.marker.bindPopup('<strong>' + recommendation.venue.name + '</strong><br><a href="' + directionsUrl + '" class="directions-text">Get directions here</a>');
 
       content.appendTo("#results");
     });
   });
+
+  callback();
 }
+
+// ip api fallback
 
 function getGeoLocation() {
   navigator.geolocation.getCurrentPosition(function(position) {
@@ -72,13 +77,20 @@ function getGeoLocation() {
   })
 }
 
+function randomizeBg() {
+  var images = ['photo-1.jpeg', 'photo-2.jpeg', 'photo-3.jpeg', 'photo-4.jpeg'];
+  $('.search').css({'background-image': 'url(img/' + images[Math.floor(Math.random() * images.length)] + ')'})
+};
+
 $('button').on('click', function(e) {
   e.preventDefault();
-  $('.search').addClass('hide');
-
-  drawResults();
+  drawResults(function() {
+    console.log('');
+  });
+  $('#search').addClass('hide');
 });
 
 $(function() {
+  // randomizeBg();
   getGeoLocation();
 });
